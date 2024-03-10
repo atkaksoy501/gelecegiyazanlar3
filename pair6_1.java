ab314e68cfb2ebb152b4151113de286789887999
Atakan Aksoy <72624958+atkaksoy501@users.noreply.github.com>
2024-03-10 11:28:07 +0300

    Update pair6_1.java

--- pair6_1.java
+++ pair6_1.java
@@ -1,3 +1,6 @@
+// Ders İçi Etkinlik
+// 09.03.2024 Pair 6 - Soru 1
+
 public class Main {
     public static void main(String[] args) {
         int num1 = 220;
@@ -8,19 +11,16 @@ public class Main {
     public static boolean isFriendNumbers(int num1, int num2) {
         int num1Total = 0;
         int num2Total = 0;
-
         for (int i = 1; i < num1; i++) {
             if (num1 % i == 0) {
                 num1Total += i;
             }
         }
-
         for (int j = 1; j < num2; j++) {
             if (num2 % j == 0) {
                 num2Total += j;
             }
         }
-
         if (num1Total == num2 && num2Total == num1) return true;
         else return false;
     }
