ed28327bdc2c9b5a2909c609dbdf9aa00c269d19
Atakan Aksoy <atakan.aksoy@std.antalya.edu.tr>
2024-03-25 20:31:49 +0300

    25.03.2024

--- src/main/java/com/turkcell/findex/api/controllers/FindexController.java
+++ src/main/java/com/turkcell/findex/api/controllers/FindexController.java
@@ -2,6 +2,8 @@ package com.turkcell.findex.api.controllers;
 
 import com.turkcell.findex.business.abstracts.FindexService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,7 @@ public class FindexController {
     private FindexService findexService;
 
     @GetMapping("/{nationalId}")
-    public int getScore(@PathVariable String nationalId) {
-        return findexService.getFindexScore(nationalId);
+    public ResponseEntity<Integer> getScore(@PathVariable String nationalId) {
+        return new ResponseEntity<>(findexService.getFindexScore(nationalId), HttpStatus.OK);
     }
 }
